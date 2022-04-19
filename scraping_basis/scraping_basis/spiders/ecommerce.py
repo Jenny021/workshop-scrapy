aeb9feea5f083ad1658f7958ffacdf1b97c1c948
Jenny Zhu <jenny.zhu@epitech.eu>
2022-04-19 15:46:00 +0200

    [ADD] added ex2 and 3 probably

--- scraping_basis/scraping_basis/spiders/ecommerce.py
+++ scraping_basis/scraping_basis/spiders/ecommerce.py
@@ -3,10 +3,16 @@ import scrapy
 class EcommerceSpider(scrapy.Spider):
     name = 'ecommerce'
     allowed_domains = ['webscraper.io']
-    start_urls = ['http://webscraper.io/']
+    start_urls = ['http://webscraper.io/test-sites/e-commerce/static']
 
     def parse(self, response):
         for info in response.css('div.thumbnail'):
             yield {
-                'name': info.css('a.title::title').get()
+                'title': info.css('h4 a::attr(title)').get(),
+                'detail_link': info.css('h4 a::attr(href)').get(),
+                'description': info.css('p.description::text').get(),
+                'price': info.css('h4::text').get(),
+                'image': info.css('img').getall(),
+                'rating': info.css('p::attr(data-rating)').get(),
+                'review count': info.css('div.ratings p.pull-right::text').get()
             }
