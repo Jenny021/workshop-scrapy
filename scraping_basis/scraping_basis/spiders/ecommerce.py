68d099e81d2af2d64fa2563d8533142776c5c39e
Jenny Zhu <jenny.zhu@epitech.eu>
2022-04-19 15:58:42 +0200

    [MOD] added ex 4 and 5?

--- scraping_basis/scraping_basis/spiders/ecommerce.py
+++ scraping_basis/scraping_basis/spiders/ecommerce.py
@@ -3,7 +3,12 @@ import scrapy
 class EcommerceSpider(scrapy.Spider):
     name = 'ecommerce'
     allowed_domains = ['webscraper.io']
-    start_urls = ['http://webscraper.io/test-sites/e-commerce/static']
+    start_urls = [
+        'http://webscraper.io/test-sites/e-commerce/static',
+        'https://webscraper.io/test-sites/e-commerce/static/computers/laptops',
+        'https://webscraper.io/test-sites/e-commerce/static/computers/tablets',
+        'https://webscraper.io/test-sites/e-commerce/static/phones/touch'
+        ]
 
     def parse(self, response):
         for info in response.css('div.thumbnail'):
@@ -16,3 +21,7 @@ class EcommerceSpider(scrapy.Spider):
                 'rating': info.css('p::attr(data-rating)').get(),
                 'review count': info.css('div.ratings p.pull-right::text').get()
             }
+        next_page = response.css('li.page-item a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
